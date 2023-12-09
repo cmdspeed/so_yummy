4b96c939edf445c50cf7f32ff0542348b3f941cf
cmdspeed <cmdspeed@gmail.com>
2023-12-09 19:09:34 +0100

    edit navigate site if user not logged

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -18,7 +18,7 @@ export const register = createAsyncThunk(
   async (credentials, thunkAPI) => {
     try {
       const res = await axios.post("/auth/signup", credentials);
-      // After successful registration, add the token to the HTTP header
+
       setAuthHeader(res.data.data.token);
       return res.data.data;
     } catch (error) {
@@ -32,7 +32,7 @@ export const logIn = createAsyncThunk(
   async (credentials, thunkAPI) => {
     try {
       const res = await axios.post("/auth/login", credentials);
-      // After successful login, add the token to the HTTP header
+
       console.log("credentials ", res.data.data.token);
       setAuthHeader(res.data.data.token);
       return res.data.data;
@@ -45,7 +45,7 @@ export const logIn = createAsyncThunk(
 export const logOut = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
   try {
     await axios.post("/auth/logout");
-    // After a successful logout, remove the token from the HTTP header
+
     clearAuthHeader();
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
@@ -55,17 +55,14 @@ export const logOut = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
 export const refreshUser = createAsyncThunk(
   "auth/refresh",
   async (_, thunkAPI) => {
-    // Reading the token from the state via getState()
     const state = thunkAPI.getState();
     const persistedToken = state.auth.token;
 
     if (persistedToken === null) {
-      // If there is no token, exit without performing any request
       return thunkAPI.rejectWithValue("Unable to fetch user");
     }
 
     try {
-      // If there is a token, add it to the HTTP header and perform the request
       setAuthHeader(persistedToken);
       const res = await axios.get("/auth/current");
 
