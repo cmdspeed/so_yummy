20aca8a736f92e885262b86c945fa708b3553b35
cmdspeed <cmdspeed@gmail.com>
2023-12-11 16:27:37 +0100

    add tag nav

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -3,29 +3,31 @@ import icons from "../../../assets/icons.svg";
 
 export const Navigation = () => {
   return (
-    <ul>
-      <li>
-        <NavLink to="/categories/beef">Categories</NavLink>
-      </li>
-      <li>
-        <NavLink to="/add">Add recipes</NavLink>
-      </li>
-      <li>
-        <NavLink to="/my">My recipes</NavLink>
-      </li>
-      <li>
-        <NavLink to="/favorite">Favorites</NavLink>
-      </li>
-      <li>
-        <NavLink to="/shopping-list">Shopping list</NavLink>
-      </li>
-      <li>
-        <NavLink to="/search">
-          <svg width="24" height="24">
-            <use href={`${icons}#search`} />
-          </svg>
-        </NavLink>
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/categories/beef">Categories</NavLink>
+        </li>
+        <li>
+          <NavLink to="/add">Add recipes</NavLink>
+        </li>
+        <li>
+          <NavLink to="/my">My recipes</NavLink>
+        </li>
+        <li>
+          <NavLink to="/favorite">Favorites</NavLink>
+        </li>
+        <li>
+          <NavLink to="/shopping-list">Shopping list</NavLink>
+        </li>
+        <li>
+          <NavLink to="/search">
+            <svg width="24" height="24">
+              <use href={`${icons}#search`} />
+            </svg>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
