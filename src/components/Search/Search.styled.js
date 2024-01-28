c0e28c1868fe3185f6c4f28f3ff0e5cdce1d3d5a
cmdspeed <cmdspeed@gmail.com>
2024-01-28 16:21:19 +0100

    fix search button positon

--- src/components/Search/Search.styled.js
+++ src/components/Search/Search.styled.js
@@ -2,23 +2,24 @@ import styled from "styled-components";
 
 export const SearchInput = styled.input`
   position: relative;
-  width: 11.31rem;
+  width: 100%;
   height: 3.25rem;
   border-radius: 22px 44px;
-  /* border: none; */
-  padding: 1.06rem 2rem;
+
+  padding: 1.06rem 3rem 1.06rem 2rem;
   &::placeholder {
     color: rgba(240, 240, 240, 1);
   }
 `;
 export const ContainerButton = styled.div`
-  position: relative;
-  right: 3rem;
-  width: 7.06rem;
+  margin-left: -3rem;
+  z-index: 1;
+  width: 11rem;
   height: 3.25rem;
 `;
 
 export const Wrapper = styled.div`
+  margin: 0 auto;
   display: flex;
-  width: 18.43rem;
+  width: 16.43rem;
 `;
