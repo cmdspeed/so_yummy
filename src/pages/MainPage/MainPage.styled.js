9db63d8ba70f4aaa67c1602f168b972b80c5700c
cmdspeed <cmdspeed@gmail.com>
2024-01-29 13:30:29 +0100

    add bg image

--- src/pages/MainPage/MainPage.styled.js
+++ src/pages/MainPage/MainPage.styled.js
@@ -21,8 +21,28 @@ export const Description = styled.p`
   font-size: 0.875rem;
   font-weight: 400;
   line-height: 1.28;
-
+  letter-spacing: -0.0175rem;
   max-width: 15.5rem;
 
   margin: 0 auto 2.75rem;
+`;
+
+export const StyledImagePasta = styled.img`
+  position: absolute;
+  left: 0;
+  top: 2rem;
+`;
+
+export const StyledImagePasta2 = styled.img`
+  position: absolute;
+  right: 0;
+  top: 2rem;
+  z-index: -1;
+`;
+
+export const StyledBackground = styled.img`
+  position: absolute;
+  right: -2rem;
+  top: 8rem;
+  z-index: -2;
 `;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,19 +1,32 @@
 import { Main } from "../../components/Main/Main";
-import { BrandName, Description, Highlighted } from "./MainPage.styled";
+import {
+  BrandName,
+  Description,
+  Highlighted,
+  StyledBackground,
+  StyledImagePasta,
+  StyledImagePasta2,
+} from "./MainPage.styled";
+
+import kisspngPasta from "../../assets/img/mainPage/kisspng-pasta.webp";
+import kisspngPasta2 from "../../assets/img/mainPage/kisspng-pasta2.webp";
+import kisspngPastaBg from "../../assets/img/mainPage/kisspng-pasta-bg.webp";
 
 const MainPage = () => {
   return (
     <>
-      <div>
-        <BrandName>
-          <Highlighted>So</Highlighted>Yummy
-        </BrandName>
+      <StyledImagePasta src={`${kisspngPasta}`} alt="kisspngPasta" />
+      <StyledImagePasta2 src={`${kisspngPasta2}`} alt="kisspngPasta" />
+      <StyledBackground src={`${kisspngPastaBg}`} alt="kisspngPasta" />
 
-        <Description>
-          "What to cook?" is not only a recipe app, it is, in fact, your
-          cookbook. You can add your own recipes to save them for the future.
-        </Description>
-      </div>
+      <BrandName>
+        <Highlighted>So</Highlighted>Yummy
+      </BrandName>
+      <Description>
+        "What to cook?" is not only a recipe app, it is, in fact, your cookbook.
+        You can add your own recipes to save them for the future.
+      </Description>
+
       <Main />
     </>
   );
