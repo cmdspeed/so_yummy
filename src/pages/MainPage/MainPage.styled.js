f5ab8f7dd41f3d433c0cdb5b826fe8794c9fc588
cmdspeed <cmdspeed@gmail.com>
2024-01-29 11:51:29 +0100

    add title and description

---
+++ src/pages/MainPage/MainPage.styled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const BrandName = styled.h1`
+  color: var(--second-color);
+  text-align: center;
+  font-size: 3.75rem;
+  font-weight: 400;
+  line-height: 1;
+  letter-spacing: -0.018rem;
+
+  margin: 4.25rem auto 0.875rem;
+`;
+
+export const Highlighted = styled.span`
+  color: var(--brand-color);
+`;
+
+export const Description = styled.p`
+  color: var(--font-description);
+  text-align: center;
+  font-size: 0.875rem;
+  font-weight: 400;
+  line-height: 1.28;
+
+  max-width: 15.5rem;
+
+  margin: 0 auto 2.75rem;
+`;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,22 @@
 import { Main } from "../../components/Main/Main";
+import { BrandName, Description, Highlighted } from "./MainPage.styled";
 
 const MainPage = () => {
-  return <Main />;
+  return (
+    <>
+      <div>
+        <BrandName>
+          <Highlighted>So</Highlighted>Yummy
+        </BrandName>
+
+        <Description>
+          "What to cook?" is not only a recipe app, it is, in fact, your
+          cookbook. You can add your own recipes to save them for the future.
+        </Description>
+      </div>
+      <Main />
+    </>
+  );
 };
 
 export default MainPage;
